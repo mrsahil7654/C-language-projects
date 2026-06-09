139e93fcb4782498bdf40623a7269f1c757ac9e1
mrsahil7654 <khansahilops917@gmail.com>
2026-06-09 19:49:14 +0530

    Create area of circle.c

---
+++ area of circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//area of square
+
+int main(){
+    float redius;
+    printf("enter redius");
+    scanf("%f",&redius);
+    printf("area is : %f",3.14*redius*redius);
+
+    return 0;
+}

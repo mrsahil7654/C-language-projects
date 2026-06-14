dca705ebb22db250317860a8ee3c8c575fa385d2
mrsahil7654 <khansahilops917@gmail.com>
2026-06-14 11:36:20 +0530

    Create keep taking number as input from user until user enter a number which is multiple of 7.c

---
+++ keep taking number as input from user until user enter a number which is multiple of 7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n ;
+    do{
+        printf("enter a number:");
+        scanf("%d\n",n);
+        printf("%d\n",n);
+        
+        if (n%7==0){//multiple of 7
+        break;
+        }
+        } while(1);
+        printf("thank you");
+     
+    return 0;
+}

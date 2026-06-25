a3680b1ea02a5f2778f8f868f4d03c422a6319ae
mrsahil7654 <khansahilops917@gmail.com>
2026-06-25 17:57:06 +0530

    Create write a function to convert celsius to fahrenhait.c

---
+++ write a function to convert celsius to fahrenhait.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+float convertn temp (float celsius);
+
+int main()
+{
+    float far = convert temp (37);
+    printf("far %f " far);
+
+    return 0;
+}
+   float convert temp ( float celsius){
+       float for = celsius *(9.0/5.0)+32;
+       return for ;
+   }
